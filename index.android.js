713221aca48c7c953c8bbd1ecb40e9d7e182ff38
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-10-05 19:48:52 -0300

    Added game logical.

--- index.android.js
+++ index.android.js
@@ -14,41 +14,83 @@ import {
 } from 'react-native';
 
 
-class MeuComponente extends Component {
-  render() {
-    return  (
-      <View>
-        <Text>{this.props.teste}</Text>
-      </View>
-    );
-  }
-}
 
 class pedra_papel_tesoura_app extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = { texto : 'Texto teste padrão' };
-    this.clicks = 1;
-  }
 
-  alteraTexto() {
-    this.setState({ texto : 'Texto alterado ' + this.clicks });
-    this.clicks++;    
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = { escolhaUsuario : '', 
+			escolhaComputador : '', 
+			resultado : '' 
+		};
+	}
+
+	jokenpo(escolhaUsuario) {
+		
+		//gerar um número aleatório (0, 1 ou 2)
+		var numAleatorio = Math.floor(Math.random() * 3);
+
+		var escolhaComputador = '';
+
+		switch(numAleatorio) {
+			case 0: escolhaComputador = 'pedra'; break;
+			case 1: escolhaComputador = 'papel'; break;
+			case 2: escolhaComputador = 'tesoura'; break;
+		}
+
+		var resultado = '';
+
+		if (escolhaComputador == 'pedra') {
+			if(escolhaUsuario == 'pedra') {
+				resultado = 'Empate';
+			} else if (escolhaUsuario == 'papel') {
+				resultado = 'Usuário ganhou';
+			} else {
+				resultado = 'Computador ganhou';
+			}
+		}
+
+		if (escolhaComputador == 'papel') {
+			if(escolhaUsuario == 'papel') {
+				resultado = 'Empate';
+			} else if (escolhaUsuario == 'tesoura') {
+				resultado = 'Usuário ganhou';
+			} else {
+				resultado = 'Computador ganhou';
+			}
+		}
+
+		if (escolhaComputador == 'tesoura') {
+			if(escolhaUsuario == 'tesoura') {
+				resultado = 'Empate';
+			} else if (escolhaUsuario == 'pedra') {
+				resultado = 'Usuário ganhou';
+			} else {
+				resultado = 'Computador ganhou';
+			}
+		}
+
+		this.setState( { 	
+				escolhaUsuario : escolhaUsuario, 
+				escolhaComputador : escolhaComputador,
+				resultado : resultado
+
+			});
+	}
   
-  render() {
-    return (
-      <View> 
-        <MeuComponente teste={this.state.texto}></MeuComponente>
-        <Button
-          title='Botão'
-          onPress={() => { this.alteraTexto()}}
-        />
-      </View>
-      
-    );
-  }
+	render() {
+		return (
+	    	<View> 
+	  			<Text>Escolha do computador: {this.state.escolhaComputador} </Text>
+	  			<Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
+	  			<Text>Resultado: {this.state.resultado}</Text>
+	  			<Button title="pedra" onPress={ () => { this.jokenpo('pedra')}} />
+	  			<Button title="papel" onPress={ () => { this.jokenpo('papel')}} />
+	  			<Button title="tesoura" onPress={ () => { this.jokenpo('tesoura')}} />
+	      	</View>      
+	    );
+	}
 }
 
 AppRegistry.registerComponent('pedra_papel_tesoura_app', () => pedra_papel_tesoura_app);
