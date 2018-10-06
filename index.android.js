63406df16feec722112584add04ba1130b239b47
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-10-06 15:11:40 -0300

    App modularization and end of development.

--- index.android.js
+++ index.android.js
@@ -13,6 +13,8 @@ import {
   Button
 } from 'react-native';
 
+import Topo from './src/components/topo';
+import Icone from './src/components/icone';
 
 
 class pedra_papel_tesoura_app extends Component {
@@ -20,77 +22,105 @@ class pedra_papel_tesoura_app extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { escolhaUsuario : '', 
-			escolhaComputador : '', 
-			resultado : '' 
-		};
+		this.state = { escolhaUsuario: '',  escolhaComputador: '',  resultado: '' };
 	}
 
-	jokenpo(escolhaUsuario) {
-		
+	jokenpo(escolhaUsuario) {		
 		//gerar um número aleatório (0, 1 ou 2)
-		var numAleatorio = Math.floor(Math.random() * 3);
+		const numAleatorio = Math.floor(Math.random() * 3);
+		const usuarioGanhouMessage = 'Você Ganhou!';
+		const computadorGanhouMessage = 'Você perdeu!';
+		const empateMessage = 'Empatou!';
 
-		var escolhaComputador = '';
+		let escolhaComputador = '';
 
-		switch(numAleatorio) {
+		switch (numAleatorio) {
 			case 0: escolhaComputador = 'pedra'; break;
 			case 1: escolhaComputador = 'papel'; break;
 			case 2: escolhaComputador = 'tesoura'; break;
+			default: escolhaComputador = '';
 		}
 
-		var resultado = '';
+		let resultado = '';
 
-		if (escolhaComputador == 'pedra') {
-			if(escolhaUsuario == 'pedra') {
-				resultado = 'Empate';
-			} else if (escolhaUsuario == 'papel') {
-				resultado = 'Usuário ganhou';
+		if (escolhaComputador === 'pedra') {
+			if (escolhaUsuario === 'pedra') {
+				resultado = empateMessage;
+			} else if (escolhaUsuario === 'papel') {
+				resultado = usuarioGanhouMessage;
 			} else {
-				resultado = 'Computador ganhou';
+				resultado = computadorGanhouMessage;
 			}
 		}
 
-		if (escolhaComputador == 'papel') {
-			if(escolhaUsuario == 'papel') {
-				resultado = 'Empate';
-			} else if (escolhaUsuario == 'tesoura') {
-				resultado = 'Usuário ganhou';
+		if (escolhaComputador === 'papel') {
+			if (escolhaUsuario === 'papel') {
+				resultado = empateMessage;
+			} else if (escolhaUsuario === 'tesoura') {
+				resultado = usuarioGanhouMessage;
 			} else {
-				resultado = 'Computador ganhou';
+				resultado = computadorGanhouMessage;
 			}
 		}
 
-		if (escolhaComputador == 'tesoura') {
-			if(escolhaUsuario == 'tesoura') {
-				resultado = 'Empate';
-			} else if (escolhaUsuario == 'pedra') {
-				resultado = 'Usuário ganhou';
+		if (escolhaComputador === 'tesoura') {
+			if (escolhaUsuario === 'tesoura') {
+				resultado = empateMessage;
+			} else if (escolhaUsuario === 'pedra') {
+				resultado = usuarioGanhouMessage;
 			} else {
-				resultado = 'Computador ganhou';
+				resultado = computadorGanhouMessage;
 			}
 		}
 
-		this.setState( { 	
-				escolhaUsuario : escolhaUsuario, 
-				escolhaComputador : escolhaComputador,
-				resultado : resultado
-
-			});
+		this.setState( { escolhaUsuario, escolhaComputador, resultado });
 	}
   
 	render() {
 		return (
-	    	<View> 
-	  			<Text>Escolha do computador: {this.state.escolhaComputador} </Text>
-	  			<Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-	  			<Text>Resultado: {this.state.resultado}</Text>
-	  			<Button title="pedra" onPress={ () => { this.jokenpo('pedra')}} />
-	  			<Button title="papel" onPress={ () => { this.jokenpo('papel')}} />
-	  			<Button title="tesoura" onPress={ () => { this.jokenpo('tesoura')}} />
+	    	<View>
+	    		<Topo />
+	    		<View style={styles.painelAcoes}>
+	    			<View style={styles.btnEscolha}>
+	    				<Button title="pedra" onPress={() => { this.jokenpo('pedra'); }} />
+	    			</View>
+	    			<View style={styles.btnEscolha}>
+	  					<Button title="papel" onPress={() => { this.jokenpo('papel'); }} />
+	  				</View>
+	  				<View style={styles.btnEscolha}>
+	  					<Button title="tesoura" onPress={() => { this.jokenpo('tesoura'); }} />
+	  				</View>
+	    		</View>
+	    		<View style={styles.palco}>
+	    			<Text style={styles.txtResultado}>{this.state.resultado}</Text>
+	    			<Icone escolha={this.state.escolhaComputador} jogador='Computador'/>
+	    			<Icone escolha={this.state.escolhaUsuario} jogador='Usuário' />	  				
+	  			</View>	  			
 	      	</View>      
 	    );
 	}
 }
+
+const styles = StyleSheet.create({
+	btnEscolha: {
+		width: 90,
+		alignItems: 'center',
+	},
+	painelAcoes: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		marginTop: 10
+	},
+	palco: {
+		alignItems: 'center',
+		marginTop: 10
+	},
+	txtResultado: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: 'red',
+		height: 60
+	}
+});
 
 AppRegistry.registerComponent('pedra_papel_tesoura_app', () => pedra_papel_tesoura_app);
--- index.ios.js
+++ index.ios.js
@@ -13,42 +13,114 @@ import {
   Button
 } from 'react-native';
 
+import Topo from './src/components/topo';
+import Icone from './src/components/icone';
 
-class MeuComponente extends Component {
-  render() {
-    return  (
-      <View>
-        <Text>{this.props.teste}</Text>
-      </View>
-    );
-  }
-}
 
 class pedra_papel_tesoura_app extends Component {
-  
+
   constructor(props) {
     super(props);
-    this.state = { texto : 'Texto teste padrão' };
-    this.clicks = 1;
+
+    this.state = { escolhaUsuario: '',  escolhaComputador: '',  resultado: '' };
   }
 
-  alteraTexto() {
-    this.setState({ texto : 'Texto alterado ' + this.clicks });
-    this.clicks++;    
+  jokenpo(escolhaUsuario) {   
+    //gerar um número aleatório (0, 1 ou 2)
+    const numAleatorio = Math.floor(Math.random() * 3);
+    const usuarioGanhouMessage = 'Você Ganhou!';
+    const computadorGanhouMessage = 'Você perdeu!';
+    const empateMessage = 'Empatou!';
+
+    let escolhaComputador = '';
+
+    switch (numAleatorio) {
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+      default: escolhaComputador = '';
+    }
+
+    let resultado = '';
+
+    if (escolhaComputador === 'pedra') {
+      if (escolhaUsuario === 'pedra') {
+        resultado = empateMessage;
+      } else if (escolhaUsuario === 'papel') {
+        resultado = usuarioGanhouMessage;
+      } else {
+        resultado = computadorGanhouMessage;
+      }
+    }
+
+    if (escolhaComputador === 'papel') {
+      if (escolhaUsuario === 'papel') {
+        resultado = empateMessage;
+      } else if (escolhaUsuario === 'tesoura') {
+        resultado = usuarioGanhouMessage;
+      } else {
+        resultado = computadorGanhouMessage;
+      }
+    }
+
+    if (escolhaComputador === 'tesoura') {
+      if (escolhaUsuario === 'tesoura') {
+        resultado = empateMessage;
+      } else if (escolhaUsuario === 'pedra') {
+        resultado = usuarioGanhouMessage;
+      } else {
+        resultado = computadorGanhouMessage;
+      }
+    }
+
+    this.setState( { escolhaUsuario, escolhaComputador, resultado });
   }
   
   render() {
     return (
-      <View> 
-        <MeuComponente teste={this.state.texto}></MeuComponente>
-        <Button
-          title='Botão'
-          onPress={() => { this.alteraTexto()}}
-        />
-      </View>
-      
-    );
+        <View>
+          <Topo />
+          <View style={styles.painelAcoes}>
+            <View style={styles.btnEscolha}>
+              <Button title="pedra" onPress={() => { this.jokenpo('pedra'); }} />
+            </View>
+            <View style={styles.btnEscolha}>
+              <Button title="papel" onPress={() => { this.jokenpo('papel'); }} />
+            </View>
+            <View style={styles.btnEscolha}>
+              <Button title="tesoura" onPress={() => { this.jokenpo('tesoura'); }} />
+            </View>
+          </View>
+          <View style={styles.palco}>
+            <Text style={styles.txtResultado}>{this.state.resultado}</Text>
+            <Icone escolha={this.state.escolhaComputador} jogador='Computador'/>
+            <Icone escolha={this.state.escolhaUsuario} jogador='Usuário' />           
+          </View>         
+          </View>      
+      );
   }
 }
+
+const styles = StyleSheet.create({
+  btnEscolha: {
+    width: 90,
+    alignItems: 'center',
+  },
+  painelAcoes: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10
+  },
+  palco: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  txtResultado: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'red',
+    height: 60
+  }
+});
 
 AppRegistry.registerComponent('pedra_papel_tesoura_app', () => pedra_papel_tesoura_app);
